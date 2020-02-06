0b60b9d184e48e4c52157a2a28ba82fcecb25905
Shashank <snaik2064@gmail.com>
2020-02-06 23:51:03 +0530

    Styled components integration complete.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,29 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { createGlobalStyle } from "styled-components"
 
-import "./layout.css"
+//import "./layout.css"
 
 const Layout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <GlobalStyle /> {children}
+    </>
+  )
 }
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+  body {
+    font-family: 'Open Sans', sans-serif;
+    color: #262626;
+    background:#fff;
+  }
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import styled from "styled-components"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,7 +9,13 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h3>Hello from Gatsby</h3>
+    <ButtonWrapper>Click Me</ButtonWrapper>
   </Layout>
 )
+
+const ButtonWrapper = styled.button`
+  background: blue;
+  color: white;
+`
 
 export default IndexPage
