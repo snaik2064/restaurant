9c238f4598f8a6712fcee67a5ea8398b12728eae
Shashank <snaik2064@gmail.com>
2020-02-06 23:58:17 +0530

    React icons package added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import styled from "styled-components"
+import { FaBeer } from "react-icons/fa"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,13 +9,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h3>Hello from Gatsby</h3>
-    <ButtonWrapper>Click Me</ButtonWrapper>
+    <h3>
+      {" "}
+      Lets go for a <FaBeer />?{" "}
+    </h3>
   </Layout>
 )
-
-const ButtonWrapper = styled.button`
-  background: blue;
-  color: white;
-`
 
 export default IndexPage
